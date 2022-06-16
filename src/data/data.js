6bd004d4a7e680b146f03f0464950ba829f97e2a
keremersann <keremersan01@gmail.com>
2022-06-15 23:25:21 -0700

    static data file which contains objects is added

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+export const users = [
+    {
+        id:1,
+        firstName: "Kerem",
+        lastName: "Ersan",
+        age: 22,
+        city: "Balıkesir",
+        creditCardNo: "546512",
+        type: "customer"
+    },
+    {
+        id:2,
+        firstName: "Beril",
+        lastName: "Küçükefe",
+        age: 22,
+        city: "Balıkesir",
+        creditCardNo: "644182",
+        type: "customer"
+    },
+    {
+        id:3,
+        firstName: "Marco",
+        lastName: "Rose",
+        age: 46,
+        city: "Leipzig",
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id:4,
+        firstName: "De",
+        lastName: "Zerbi",
+        age: 43,
+        city: "Brescia",
+        salary: 7500,
+        type: "employee"
+    }
+] 
