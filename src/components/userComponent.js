4338f297859007c290d5f55d6fb0b365f2533c8a
keremersann <keremersan01@gmail.com>
2022-06-15 23:25:51 -0700

    component and userservice is reorganized based on new updates

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -9,11 +9,16 @@ let logger = new ElasticLogger()
 let userService = new UserService(logger)
 
 
-let user1 = new User(1,"İpek", 24)
-let user2 = new User(2,"Salim", 12)
+let user1 = new User(1,"İpek", "Yazıcı", 11, "İzmir")
+let user2 = new User(2,"Salim", "Manav", 29, "Adana")
 
 userService.add(user1)
 userService.add(user2)
 
 console.log(userService.list())
 console.log(userService.getById(1))
+console.log("---------------------------------")
+
+userService.load()
+console.log(userService.employees)
+console.log(userService.customers)
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,14 +1,33 @@
+import { users } from "../data/data.js"
+
 export default class UserService{
     constructor(loggerService){
         this.users = [], // getUsersFormDB()
+        this.employees = [],
+        this.customers = [],
         this.loggerService = loggerService
     }
 
+    load(){
+        for (const user of users) {
+            switch (user.type) {
+                case "customer":
+                    this.customers.push(user)
+                    break;
+                case "employee":
+                    this.employees.push(user)
+                    break;
+                default:
+                    console.log("User type is wrong!")
+                    break;
+            }
+        }
+    }
 
     add(user){
         this.users.push(user)
         this.loggerService.log(user)
-        console.log("User is added!: " + user.name)
+        console.log("User is added!: " + user.firstName)
     }
 
     list()
